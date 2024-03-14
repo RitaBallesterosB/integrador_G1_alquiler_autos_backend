19a930cd54c83ed4cd8631283c03c0cfc1898e2c
elida96 <elidasanchez1996@gmail.com>
2024-03-13 19:45:42 -0500

    corrección HU 6 Y endpoint vermas

--- alquiler-autos/alquiler-autos/src/main/java/com/backend/apirest/autos/alquilerautos/service/ImpImagenService.java
+++ alquiler-autos/alquiler-autos/src/main/java/com/backend/apirest/autos/alquilerautos/service/ImpImagenService.java
@@ -10,7 +10,9 @@ import java.util.List;
 public interface ImpImagenService {
     ImagenSalidaDto agregarImagen(ImagenEntradaDto imagen);
 
-    List<ImagenSalidaDto> listarGaleriaImagenes();
+    List<ImagenSalidaDto> listarGaleriaImagenes(Long vehiculoId);
+
+    List<ImagenSalidaDto> listarGaleriaImagenesVerMas(Long vehiculoId);
 
     public List<ImagenSalidaDto> listarImagenesPorVehiculo(Long vehiculoId);
 }
