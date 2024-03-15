a3b727f3de33f7c0dab22d1d8ec5323ee5e2a905
RitaBB <ritaballesterosba@gmail.com>
2024-03-15 18:35:15 -0500

    Guardar cambios en CorsConfig.java antes de fusionar

--- alquiler-autos/alquiler-autos/src/main/java/com/backend/apirest/autos/alquilerautos/CorsConfig.java
+++ alquiler-autos/alquiler-autos/src/main/java/com/backend/apirest/autos/alquilerautos/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.backend.apirest.autos.alquilerautos.config;
+package com.backend.apirest.autos.alquilerautos;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
