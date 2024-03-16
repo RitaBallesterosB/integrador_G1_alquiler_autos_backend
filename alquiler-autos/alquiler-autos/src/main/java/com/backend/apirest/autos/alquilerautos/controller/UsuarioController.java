afc58f30e6b2fd2e95db8ce6c3f90e7817fdb127
RitaBB <ritaballesterosba@gmail.com>
2024-03-16 00:56:30 -0500

    registrar usuario ajustado HU13

--- alquiler-autos/alquiler-autos/src/main/java/com/backend/apirest/autos/alquilerautos/controller/UsuarioController.java
+++ alquiler-autos/alquiler-autos/src/main/java/com/backend/apirest/autos/alquilerautos/controller/UsuarioController.java
@@ -1,32 +1,61 @@
 package com.backend.apirest.autos.alquilerautos.controller;
 
 import com.backend.apirest.autos.alquilerautos.dto.entrada.usuario.UsuarioEntradaDto;
+import com.backend.apirest.autos.alquilerautos.dto.salida.usuario.UsuarioSalidaDto;
+import com.backend.apirest.autos.alquilerautos.entity.Usuario;
+import com.backend.apirest.autos.alquilerautos.exceptions.UsuarioNotFoundException;
+import com.backend.apirest.autos.alquilerautos.repository.UsuarioRepository;
 import com.backend.apirest.autos.alquilerautos.service.impl.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+
 
 @RestController
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
+    @Autowired
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+
 
     @PostMapping("/usuarios/registro")
-    public ResponseEntity<String> registrarUsuario(@RequestBody UsuarioEntradaDto usuarioDto) {
-        System.out.println("Solicitud recibida para registrar usuario: " + usuarioDto);
-        usuarioService.registrarUsuario(usuarioDto);
-        System.out.println("Usuario registrado exitosamente: " + usuarioDto);
-        return ResponseEntity.ok("Usuario registrado exitosamente");
+    public ResponseEntity<UsuarioSalidaDto> registrarUsuario(@RequestBody UsuarioEntradaDto usuarioDto) {
+        // Registra el usuario y obtén su ID
+        Long idUsuarioRegistrado = usuarioService.registrarUsuario(usuarioDto);
+
+        // Construye el UsuarioSalidaDto con el ID del usuario registrado y los datos proporcionados en el request
+        UsuarioSalidaDto usuarioSalidaDto = new UsuarioSalidaDto();
+        usuarioSalidaDto.setIdUsuario(idUsuarioRegistrado);
+        usuarioSalidaDto.setNombre(usuarioDto.getNombre());
+        usuarioSalidaDto.setApellido(usuarioDto.getApellido());
+        usuarioSalidaDto.setCorreoElectronico(usuarioDto.getCorreoElectronico());
+
+        // Retorna el UsuarioSalidaDto
+        return ResponseEntity.ok(usuarioSalidaDto);
     }
 
-    @PostMapping("/administrar")
-    public ResponseEntity<Void> administrarUsuario(@RequestParam Long idUsuario, @RequestParam boolean isAdmin) {
-        usuarioService.administrarUsuario(idUsuario, isAdmin);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
